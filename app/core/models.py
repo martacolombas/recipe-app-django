6e6fdfb2a2618c3d946c84a9eeb03400f4b42507
Marta <marta.colombas@travelperk.com>
2020-05-24 18:39:36 +0200

    feat: added superuser capabilities in user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
 # Create your models here.
 
 
@@ -7,10 +8,21 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """ creates and saves a new user """
+        if not email:
+            raise ValueError("Users must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         """ this is required to support multiple databases """
+
+        return user
+
+    def create_superuser(self, email, password):
+        """ Creates and saves a new superuser """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
         return user
 
